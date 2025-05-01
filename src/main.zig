89142fc75a853d7e214b5aa352e292bbbca5cde3
MiloAC1 <crabapplefarms@icloud.com>
2025-05-01 05:59:24 -0500

    Added default gaussian

--- src/main.zig
+++ src/main.zig
@@ -69,6 +69,7 @@ fn resize(allocator: std.mem.Allocator, img: stb_image.Image) !stb_image.Image {
 
     const linear = try luminize(imgW, imgH, imgC, imgP, allocator);
 
+    //Downscaler
     for (0..new_w) |x| {
         for (0..new_h) |y| {
             const originX = @divFloor(x * imgW, new_w);
@@ -81,12 +82,12 @@ fn resize(allocator: std.mem.Allocator, img: stb_image.Image) !stb_image.Image {
             }
         }
     }
+    //Upscaler
     for (0..new_w) |x| {
         for (0..new_h) |y| {
             const oX = @divFloor(x * imgW, new_w);
             const oY = @divFloor(y * imgH, new_h);
             const index = ((y * new_w + x) * new_c);
-            std.debug.print("x is {}\n", .{x});
 
             for (0..8) |w| {
                 for (0..8) |h| {
@@ -102,12 +103,74 @@ fn resize(allocator: std.mem.Allocator, img: stb_image.Image) !stb_image.Image {
         }
     }
 
-    rimg.pixels = if (upscale.len != 0) &upscale[0] else null;
+    const gauss = try gaussian(imgW, imgH, imgC, linear, allocator);
+    rimg.pixels = if (gauss.len != 0) &gauss[0] else null;
     return rimg;
 }
-fn luminize(w: usize, h: usize, c: usize, p: []u8, alloc: std.mem.Allocator) ![]u8 {
+fn gaussian(w: usize, h:usize, c: usize, p: []u8, allocator: std.mem.Allocator) ![]u8 {
+    const size = w * h * c;
+    var gauss: []u8 = try allocator.alloc(u8, size);
+    const sigma: f32 = 2;
+    const K = try kernal(sigma); 
+    var conv: f32 = undefined;
+
+    for (0..h) |y| {
+        for (0..w) |x| {
+            const index = ((y * w + x) * c);
+
+            //Calculate convolution
+            for (0..c) |channel| {
+
+                conv = 0.0;
+                for (K, 0..) |row, i| {
+                    for (row, 0..) |value, j| {
+                        const si: i32 = @intCast(i);
+                        const sj: i32 = @intCast(j);
+                        const sx: i32 = @intCast(x);
+                        const sy: i32 = @intCast(y);
+
+                        const sw: i32 = @intCast(w);
+                        const sc: i32 = @intCast(c);
+
+                        const x2: i32 = si - 3;
+                        const y2: i32 = sj - 3;
+
+                        const nx: i32 = sx + x2;
+                        const ny: i32 = sy + y2;
+                        
+                        const schannel: i32 = @intCast(channel);
+                        //std.debug.print("si = {}\n",.{si});
+                        //std.debug.print("sj = {}\n",.{sj});
+                        if (nx >= 0 and ny >= 0 and nx < w and ny < h) {
+
+                            const gi: usize = @intCast((ny * sw + nx) * sc + schannel);
+                            const pv: f32 = @floatFromInt(p[gi]);
+
+                            //std.debug.print("nx = {}\n",.{nx});
+                            //std.debug.print("ny = {}\n",.{ny});
+                            //std.debug.print("gi = {}\n",.{gi});
+                            //std.debug.print("pv = {}\n",.{pv});
+                            conv += pv * value; 
+                            //std.debug.print("conv = {}\n",.{conv});
+
+                        }
+
+                    }
+                }
+                const clamp_conv = std.math.clamp(conv,0.0, 255.0);
+                gauss[index + channel] = @intFromFloat(clamp_conv);
+            }
+
+        }
+    }
+    for (0..20) |i| {
+        std.debug.print("in[{}]={} out = {}\n",.{i, p[i],gauss[i]});
+    }
+    return gauss;
+}
+fn luminize(w: usize, h: usize, c: usize, p: []u8, allocator: std.mem.Allocator) ![]u8 {
     const pc = w * h * c;
-    const linearized: []u8 = try alloc.alloc(u8, pc);
+    const luminized: []u8 = try allocator.alloc(u8, pc);
     for (0..w) |x| {
         for (0..h) |y| {
             const indexL = ((y * w + x) * c);
@@ -119,16 +182,44 @@ fn luminize(w: usize, h: usize, c: usize, p: []u8, alloc: std.mem.Allocator) ![]
             const vG: f32 = linearize(g / 255.0);
             const vB: f32 = linearize(b / 255.0);
             const lum = vR * 0.2126 + vG * 0.7152 + vB * 0.0722;
-            const constrained = @floor(lum * 10) / 10;
+            //const constrained = @floor(lum * 10) / 10;
 
             for (0..c) |i| {
-                linearized[indexL + i] = @intFromFloat(std.math.clamp( constrained * 255, 0, 255));
+                //Use lum, or constrained for banding
+                luminized[indexL + i] = @intFromFloat(std.math.clamp( lum * 255, 0, 255));
             }
         }
     }
-    return linearized;
+    return luminized;
 }
+fn kernal(sigma: f32) ![7][7]f32 {
+    
+    var K: [7][7]f32 = undefined;
+    var constraint: f32 = undefined;
+    for (K, 0..) |row, i| {
+        for (row, 0..) |_, j| {
+            const x: i32 = @intCast(i);
+            const y: i32 = @intCast(j);
+            const nx: i32 = x - 3;
+            const ny: i32 = y - 3;
+            const pvalue = 1/(2 * std.math.pi * sigma * sigma) * std.math.exp(-(std.math.pow(f32,@floatFromInt(nx),2) + std.math.pow(f32,@floatFromInt(ny),2))/2 * sigma);
 
+            K[i][j] = pvalue;
+            constraint += pvalue;
+            //std.debug.print("row is {}\n",.{row});
+            std.debug.print("pvalue is {}\n",.{pvalue});
+        }
+    }
+    for (K, 0..) |row, i| {
+        for (row, 0..) |value, j| {
+            K[i][j] = value / constraint;
+            std.debug.print("v/c = {}\n",.{value/constraint});
+        }
+    }
+    
+return K;
+
+}
 fn srgbize(c: f32) f32 {
     if (c <= 0.0031308) {
         return c * 12.92;
